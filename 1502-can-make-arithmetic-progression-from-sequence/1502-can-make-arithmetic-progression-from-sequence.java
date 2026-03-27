1da5aa7ea271b1ac6f27d37c7748d0de1186e932
khushilorish <lorishkhushi@gmail.com>
2026-03-27 19:28:58 +0530

    Time: 1 ms (98.84%), Space: 44.1 MB (67.28%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -1,12 +1,56 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
-        Arrays.sort(arr);
-        int d = arr[1]- arr[0];
-        for(int i=1; i<arr.length-1; i++){
-            if(arr[i+1] - arr[i] != d){
+        mergeSort(arr, 0, arr.length - 1);
+
+        int diff = arr[1] - arr[0];
+
+        for (int i = 1; i < arr.length - 1; i++) {
+            if (arr[i + 1] - arr[i] != diff) {
                 return false;
             }
         }
+
         return true;
+    }
+
+    // Merge Sort
+    private void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) return;
+
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+
+    private void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        // copy back
+        for (int p = 0; p < temp.length; p++) {
+            arr[left + p] = temp[p];
+        }
     }
 }
