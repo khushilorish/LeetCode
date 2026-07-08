3f1b28651bb67b7030f7213bf5c017f6cf292302
KHUSHI LORISH <lorishkhushi@gmail.com>
2026-07-08 20:04:04 +0530

    Time: 0 ms (100%), Space: 42.2 MB (48.99%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int binaryGap(int n) {
+        int prev = -1;
+        int max = 0;
+        int pos = 0;
+
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                if (prev != -1) {
+                    max = Math.max(max, pos - prev);
+                }
+                prev = pos;
+            }
+
+            n >>= 1;
+            pos++;
+        }
+
+        return max;
+    }
+}
